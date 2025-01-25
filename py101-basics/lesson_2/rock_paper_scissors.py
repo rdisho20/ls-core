1bfa6ed39d4008eac7b7804e73ebfd73b5ee210b
rdisho20 <xnod_bs@yahoo.com>
2025-01-25 13:04:03 -0600

    bonus feature 1 for RPS game complete

--- py101-basics/lesson_2/rock_paper_scissors.py
+++ py101-basics/lesson_2/rock_paper_scissors.py
@@ -1,6 +1,9 @@
+# Bonus Features
+# given collection :: choices
+
 import random
 
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 def prompt(message):
     print(f"==> {message}")
@@ -8,13 +11,27 @@ def prompt(message):
 def display_winner(choice, computer_choice):
     prompt(f"You chose {choice} and the computer chose {computer_choice}")
 
-    if ((choice == "rock" and computer_choice == "scissors") or
-        (choice == "paper" and computer_choice == "rock") or
-        (choice == "scissors" and computer_choice == "paper")):
+    if ((choice == "rock" and (computer_choice == "scissors" or
+                               computer_choice == "lizard")) or
+        (choice == "paper" and (computer_choice == "rock" or
+                                computer_choice == "spock")) or
+        (choice == "scissors" and (computer_choice == "paper" or
+                                   computer_choice == "lizard")) or
+        (choice == "lizard" and (computer_choice == "spock" or
+                                 computer_choice == "paper")) or
+        (choice == "spock" and (computer_choice == "scissors" or
+                                computer_choice == "rock"))):
         prompt("You win!")
-    elif ((choice == "rock" and computer_choice == "paper") or
-        (choice == "paper" and computer_choice == "scissors") or
-        (choice == "scissors" and computer_choice == "rock")):
+    elif ((computer_choice == "rock" and (choice == "scissors" or
+                                          choice == "lizard")) or
+        (computer_choice == "paper" and (choice == "rock" or
+                                         choice == "spock")) or
+        (computer_choice == "scissors" and (choice == "paper" or
+                                            choice == "lizard")) or
+        (computer_choice == "lizard" and (choice == "spock" or
+                                          choice == "paper")) or
+        (computer_choice == "spock" and (choice == "scissors" or
+                                         choice == "rock"))):
         prompt("Computer wins!")
     else:
         prompt("It's a tie")
