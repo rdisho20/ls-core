ac5caa2b8f6e4d9b1572ac9093f784a611490c79
rdisho20 <xnod_bs@yahoo.com>
2025-05-14 10:58:27 -0500

    adding oop practice problem file

---
+++ py120-oop/practice-problems/hard_1/2.py
@@ -0,0 +1,82 @@
+class FueledVehicleMixin:
+    def range(self):
+        if (self.__class__.__name__ == 'Catamaran' or
+            self.__class__.__name__ == 'Motorboat'):
+            return self.fuel_capacity * self.fuel_efficiency
+
+        return self.fuel_capacity * self.fuel_efficiency
+
+    def set_fuel_efficiency(self, kilometers_per_liter):
+        self.fuel_efficiency = kilometers_per_liter
+
+    def set_fuel_capacity(self, liters):
+        self.fuel_capacity = liters
+
+class WheeledVehicle(FueledVehicleMixin):
+
+    def __init__(self,
+                 tire_list,
+                 kilometers_per_liter,
+                 liters_of_fuel_capacity):
+        self.tires = tire_list
+        self.set_fuel_efficiency(kilometers_per_liter)
+        self.set_fuel_capacity(liters_of_fuel_capacity)
+
+    def tire_pressure(self, tire_index):
+        self.tires[tire_index]
+
+    def inflate_tire(self, tire_index, pressure):
+        self.tires[tire_index] = pressure
+
+class Watercraft(FueledVehicleMixin):
+    def __init__(self,
+                 number_propellers,
+                 number_hulls,
+                 kilometers_per_liter,
+                 liters_of_fuel_capacity):
+        self.propellers = number_propellers
+        self.hulls = number_hulls
+        self.set_fuel_efficiency(kilometers_per_liter)
+        self.set_fuel_capacity(liters_of_fuel_capacity)
+
+class Motorboat(Watercraft):
+    def __init__(self, kilometers_per_liter, liters_of_fuel_capacity):
+        super().__init__(1, 1, kilometers_per_liter, liters_of_fuel_capacity)
+
+class Catamaran(Watercraft):
+    def __init__(self,
+                 number_propellers,
+                 number_hulls,
+                 kilometers_per_liter,
+                 liters_of_fuel_capacity):
+        super().__init__(number_propellers,
+                         number_hulls,
+                         kilometers_per_liter,
+                         liters_of_fuel_capacity)
+
+class Auto(WheeledVehicle):
+    def __init__(self):
+        # 4 tires with various tire pressures
+        super().__init__([30, 30, 32, 32], 50, 25.0)
+
+class Motorcycle(WheeledVehicle):
+    def __init__(self):
+        # 2 tires with various tire pressures
+        super().__init__([20, 20], 80, 8.0)
+
+
+auto = Auto()
+motorcycle = Motorcycle()
+catamaran = Catamaran(2, 2, 1.5, 600)
+
+print(auto.fuel_efficiency)             # 50
+print(auto.fuel_capacity)               # 25.0
+print(auto.range())                     # 1250.0
+
+print(motorcycle.fuel_efficiency)       # 80
+print(motorcycle.fuel_capacity)         # 8.0
+print(motorcycle.range())               # 640.0
+
+print(catamaran.fuel_efficiency)        # 1.5
+print(catamaran.fuel_capacity)          # 600
+print(catamaran.range())                # 900.0
